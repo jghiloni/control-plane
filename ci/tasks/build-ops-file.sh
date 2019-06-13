8642d562ec70424e94877722a64a5b12e46afdb5
Josh Ghiloni <jghiloni@pivotal.io>
2019-06-13 12:52:08 -0600

    hopefully done with ops file building

--- ci/tasks/build-ops-file.sh
+++ ci/tasks/build-ops-file.sh
@@ -25,8 +25,10 @@ write_ops() {
 }
 
 cidrhost() {
-    echo "cidrhost($1, $2)" | "${DIR}"/terraform/terraform console)
+    echo "cidrhost($1, $2)" | "${DIR}"/terraform/terraform console
 }
+
+sgID=$(get_value '.vms_security_group_id.value')
 
 numAZs=$(get_value '.azs.value | length')
 for i in $(seq 0 $((numAZs-1))); do
@@ -36,7 +38,7 @@ done
 write_ops "/iaas-configurations/name=default/iam_instance_profile" "$(get_value '.ops_manager_iam_instance_profile_name.value')"
 write_ops "/iaas-configurations/name=default/key_pair_name" "$(get_value '.ops_manager_ssh_public_key_name.value')"
 write_ops "/iaas-configurations/name=default/region" "$(get_value '.region.value')"
-write_ops "/iaas-configurations/name=default/security_group" "$(get_value '.ops_manager_security_group_id.value')"
+write_ops "/iaas-configurations/name=default/security_group" "${sgID}"
 write_ops "/iaas-configurations/name=default/ssh_private_key" "$(get_value '.ops_manager_ssh_private_key.value')"
 
 write_ops "/network-assignment/singleton_availability_zone/name" "$(get_value '.azs.value[0]')"
@@ -56,7 +58,7 @@ vpc_cidr=$(cat "${DIR}"/source/config/terraform/terraform.tfvars | grep 'vpc_cid
 set -euo pipefail
 
 dns=$(cidrhost "${vpc_cidr}" 2)
-for network in pas infrastructure public; do
+for network in pas infrastructure services; do
     for i in $(seq 0 $((numAZs-1))); do
         az=$(get_value --argjson i $i '.azs.value[$i]')
         cidr=$(get_value --arg network $network --argjson i $i '.[$network+"_subnet_cidrs"].value[$i]')
@@ -67,8 +69,9 @@ for network in pas infrastructure public; do
         midIP=$(cidrhost "${cidr}" 10)
         lastIP=$(cidrhost "${cidr}" -1)
         reservedRanges="$firstIP-$midIP,$lastIP"
-        subnet=$(echo '{}' | jq --argjson az "$az" \
+        subnet=$(echo '{}' | $jq --argjson az "$az" \
             --argjson cidr "$cidr" \
+            --arg dns "$dns" \
             --argjson gateway "$gateway" \
             --argjson subnetId "$subnetId" \
             --arg reservedRanges "$reservedRanges" \
@@ -77,5 +80,23 @@ for network in pas infrastructure public; do
         write_ops "/networks-configuration/networks/name=$network/subnets/-" "${subnet}"
     done
 done
+
+rdsHost=$(get_value -r '.rds_address.value')
+if [[ "${rdsHost}" != "" ]]; then
+    rdsCAs=$(curl -L https://s3.amazonaws.com/rds-downloads/rds-combined-ca-bundle.pem)
+    write_ops "/properties-configuration/security_configuration/trusted_certificates" "\"${rdsCAs}\""
+fi
+
+write_ops "/vmextensions-configuration/name=web-extension/cloud_properties/lb_target_groups" "$(get_value '.web_target_groups.value')"
+write_ops "/vmextensions-configuration/name=ssh-extension/cloud_properties/lb_target_groups" "$(get_value '.ssh_target_groups.value')"
+write_ops "/vmextensions-configuration/name=tcp-extension/cloud_properties/lb_target_groups" "$(get_value '.tcp_target_groups.value')"
+
+write_ops "/vmextensions-configuration/name=web-extension/cloud_properties/security_groups/-" "${sgID}"
+write_ops "/vmextensions-configuration/name=ssh-extension/cloud_properties/security_groups/-" "${sgID}"
+write_ops "/vmextensions-configuration/name=tcp-extension/cloud_properties/security_groups/-" "${sgID}"
+
+write_ops "/vmextensions-configuration/name=web-extension/cloud_properties/security_groups/-" "\"${ENV_NAME}-lb-security-group\""
+write_ops "/vmextensions-configuration/name=ssh-extension/cloud_properties/security_groups/-" "\"${ENV_NAME}-ssh-lb-security-group\""
+write_ops "/vmextensions-configuration/name=tcp-extension/cloud_properties/security_groups/-" "\"${ENV_NAME}-tcp-lb-security-group\""
 
 cat "${DIR}"/vars/ops.yml
