50a3ebe1c32b3bb123bb38f33b9f43a0431df949
Josh Ghiloni <jghiloni@pivotal.io>
2019-06-13 11:49:32 -0600

    incremental progress

--- ci/tasks/build-ops-file.sh
+++ ci/tasks/build-ops-file.sh
@@ -21,16 +21,17 @@ write_ops() {
     local value=$2
 
     block=$(echo "{}" | $jq --arg path $path --argjson value "$value" '. |= . + {"type": "replace","path":$path,"value":$value}')
-    echo "- ${block}" | bosh int -  ${DIR}/vars/ops.yml
+    echo "- ${block}" | bosh int - >> ${DIR}/vars/ops.yml
 }
 
-write_ops "/iaas-configurations/name=default/iam_instance_profile" "$(get_value -r '.ops_manager_iam_instance_profile_name.value')"
-write_ops "/iaas-configurations/name=default/key_pair_name" "$(get_value -r '.ops_manager_ssh_public_key_name.value')"
-write_ops "/iaas-configurations/name=default/region" "$(get_value -r '.region.value')"
-write_ops "/iaas-configurations/name=default/security_group" "$(get_value -r '.ops_manager_security_group_id.value')"
+write_ops "/iaas-configurations/name=default/iam_instance_profile" "$(get_value '.ops_manager_iam_instance_profile_name.value')"
+write_ops "/iaas-configurations/name=default/key_pair_name" "$(get_value '.ops_manager_ssh_public_key_name.value')"
+write_ops "/iaas-configurations/name=default/region" "$(get_value '.region.value')"
+write_ops "/iaas-configurations/name=default/security_group" "$(get_value '.ops_manager_security_group_id.value')"
 write_ops "/iaas-configurations/name=default/ssh_private_key" "$(get_value '.ops_manager_ssh_private_key.value')"
+
 numAZs=$(get_value '.azs.value | length')
 for i in $(seq 0 $((numAZs-1))); do
-
-
+    write_ops "/az-configuration/-" "{\"name\": $(get_value --argjson i $i '.azs.value[$i]')}"
+done
 cat "${DIR}"/vars/ops.yml
