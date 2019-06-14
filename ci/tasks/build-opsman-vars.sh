ef00f3c8c210a7539f112d13bb3d7173c3f6b64a
Josh Ghiloni <jghiloni@pivotal.io>
2019-06-14 09:58:26 -0600

    Cleanup

--- ci/tasks/build-opsman-vars.sh
+++ ci/tasks/build-opsman-vars.sh
@@ -7,29 +7,25 @@ export PATH="$(pwd)/jq":${PATH}
 chmod +x jq/jq-linux64
 
 jq=jq-linux64
-json=$(cat $(pwd)/terraform-state/terraform.tfstate)
 
-echo -n "region: " >> opsman-vars/vars.yml
-echo "${json}" | $jq -r '.modules[] | select(.outputs.region?) | .outputs.region.value' >> opsman-vars/vars.yml
+cd "${DIR}"/terraform/terraforming-pas
 
-echo "opsman-name: ${ENV_NAME}-ops-manager" >> opsman-vars/vars.yml
+tfJSON=$("${DIR}"/terraform/terraform output -json)
+get_value() {
+    echo "${tfJSON}" | $jq "$@"
+}
 
-echo -n "public-subnet-id: " >> opsman-vars/vars.yml
-echo "${json}" | $jq -r '.modules[] | select(.outputs.public_subnet_ids?) | select(.path==["root"]) | .outputs.public_subnet_ids.value[0]' >> opsman-vars/vars.yml
+echo "---" > opsman-vars/vars.yml
+write_var() {
+   echo "$@" >> opsman-vars/vars.yml
+}
 
-echo -n "opsman-security-group: " >> opsman-vars/vars.yml
-echo "${json}" | $jq -r '.modules[] | select(.outputs.ops_manager_security_group_id?) | select(.path==["root"]) | .outputs.ops_manager_security_group_id.value' >> opsman-vars/vars.yml
-
-echo "opsman-keypair-name: ${ENV_NAME}-ops-manager-key" >> opsman-vars/vars.yml
-
-echo -n "access-key-id: " >> opsman-vars/vars.yml
-echo "${json}" | $jq -r '.modules[] | select(.outputs.ops_manager_iam_user_access_key?) | select(.path==["root"]) | .outputs.ops_manager_iam_user_access_key.value' >> opsman-vars/vars.yml
-
-echo -n "secret-access-key: " >> opsman-vars/vars.yml
-echo "${json}" | $jq -r '.modules[] | select(.outputs.ops_manager_iam_user_secret_key?) | select(.path==["root"]) | .outputs.ops_manager_iam_user_secret_key.value' >> opsman-vars/vars.yml
-
-
-echo -n "opsman-public-ip: " >> opsman-vars/vars.yml
-echo "${json}" | $jq -r '.modules[] | select(.outputs.ops_manager_public_ip?) | select(.path==["root"]) | .outputs.ops_manager_public_ip.value' >> opsman-vars/vars.yml
-
-echo "environment: ${ENV_NAME}" >> opsman-vars/vars.yml
+write_var "region: $(get_value '.region.value')"
+write_var "opsman-name: ${ENV_NAME}-ops-manager"
+write_var "public-subnet-id: $(get_value '.public_subnet_ids.value[0]')"
+write_var "opsman-security-group: $(get_value '.ops_manager_security_group_id.value')"
+write_var "opsman-keypair-name: ${ENV_NAME}-ops-manager-key"
+write_var "access-key-id: $(get_value '.ops_manager_iam_user_access_key.value')"
+write_var "secret-access-key: $(get_value '.ops_manager_iam_user_secret_key.value')"
+write_var "opsman-public-ip: $(get_value '.ops_manager_public_ip.value')"
+write_var "environment: ${ENV_NAME}"
